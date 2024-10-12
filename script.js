d62eb930ac06a0a53c2999cedc8be4ce694f115a
Muhammad Aurif <muhammadaurifsolangixwave@gmail.com>
2024-10-12 13:15:48 +0500

    Home Section: Responsiveness & Bug Fixes

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const scrollTopBtn = document.getElementById('scrollTopBtn');
-const homeImages = document.querySelector('img'); // Detect image element
+document.getElementById('sticky-btn').addEventListener('click', function () {
 
-window.onscroll = function () {
-  // Show the button when scrolled down 200px
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    scrollTopBtn.style.display = "flex"; // Show button
-  } else {
-    scrollTopBtn.style.display = "none"; // Hide button
-  }
-  
-  // Hide button when images come into view
-  const imagePosition = homeImages.getBoundingClientRect().top;
-  if (imagePosition < window.innerHeight && imagePosition > 0) {
-    scrollTopBtn.style.display = "none"; // Hide when image is in view
-  }
-};
-
-// Scroll to top function
-scrollTopBtn.addEventListener('click', function () {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 });
