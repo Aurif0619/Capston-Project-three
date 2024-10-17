92da1753378c68c975a641bac0b5428e32d24877
Muhammad Aurif <muhammadaurifsolangixwave@gmail.com>
2024-10-17 15:28:12 +0500

    Bootstrap alert added

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,13 +2,17 @@ document.querySelector(".loginForm").addEventListener("submit", function (event)
     event.preventDefault();
 
     let userEmailLogin = document.getElementById('userEmailLogin').value;
+    
     let userPasswordLogin = document.getElementById('userPassword').value;
 
     let storedEmail = localStorage.getItem('Email');
+
     let storedPassword = localStorage.getItem('password');
 
     if (userEmailLogin === storedEmail && userPasswordLogin === storedPassword) {
+
         alert('Welcome to the Home page');
+
         window.location.href = "/index.html";
     } else {
         alert('Something went wrong!');
--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -4,15 +4,33 @@ document.querySelector(".signUpForm")
 
         let userEmail = document.getElementById("userEmail").value;
         let userPassword = document.getElementById("userPassword").value;
+        let confirmPassword = document.getElementById("confirmPassword").value;
 
-        console.log(userEmail, userPassword);
+        console.log(userEmail, userPassword, confirmPassword);
 
-        if (userEmail && userPassword) {
-            
+        if (userEmail && userPassword && confirmPassword) {
+            if (userPassword !== confirmPassword) {
+                document.getElementById("alertMessage").innerHTML = "Password and Confirm Password do not match!";
+
+                document.getElementById("alertMessage").classList.add("alert-danger");
+                return;
+            }
+
             localStorage.setItem("Email", userEmail);
+
             localStorage.setItem("password", userPassword);
 
-            alert("Sign Up Successful!");
-            window.location.href="/pages/login/login.html"
+            document.getElementById("alertMessage").innerHTML = "Sign Up Successful!";
+
+            document.getElementById("alertMessage").classList.add("alert-success");
+
+            setTimeout(function () {
+                window.location.href = "/pages/login/login.html";
+            }, 2000);
+
+        } else {
+            document.getElementById("alertMessage").innerHTML = "Please fill out all fields!";
+
+            document.getElementById("alertMessage").classList.add("alert-danger");
         }
     });
